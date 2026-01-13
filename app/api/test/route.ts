4e74b05350aa41375c79692e1605c480cb5c3ca7
oliver-strandvall <oliver.strandvall@optistud.fi>
2026-01-13 09:46:04 +0200

    Level 1 Final

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,3 +1,3 @@
 export async function GET() {
-    return Response.json({message: "Test"});
+    return Response.json([ { "id": 1, "name": "School", "lat": 59.3, "lng": 18.1 }, { "id": 2, "name": "Library", "lat": 59.4, "lng": 18.05 } ]);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,69 @@
 "use client";
-import Image from "next/image";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [data, setData] = useState<{ id: number; name: string; lat: number; lng: number }[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   async function getData() {
-    const res = await fetch("/api/test");
-    const data = await res.json();
-    console.log(data);
+    const res =  await fetch("/api/test/");
+    console.log(res.status)
+    if(res.status === 200) {
+      setData(await res.json());
+    }
+    if(res.status !== 200){
+      setError("ERROR!");
+    }
   }
-  getData();
+
+  console.log(data);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if(error === "ERROR!") {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="bg-blue-950 font-san min-h-screen flex justify-center flex-col items-center gap-5">
+      <h1 className="text-xl font-semibold">Saved Locations:</h1>
+      <div className="bg-red-900 rounded-lg w-110 h-15 flex items-center justify-center">
+        <h1 className="text-xl font-semibold">Error loading data</h1>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
+            <h1 className="text-xl font-semibold mb-20"></h1>
+            <p></p>
+            <p></p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
+          <h1 className="text-xl font-semibold mb-20"></h1>
+          <p></p>
+          <p></p>
         </div>
-      </main>
-    </div>
+      </div>
+      <p>Data from API-route</p>
+    </main>
+  );
+  }
+
+  return (
+    <main className="bg-blue-950 font-san min-h-screen flex justify-center flex-col items-center gap-5">
+      <h1 className="text-xl font-semibold">Saved Locations:</h1>
+      <div className="flex items-center justify-center">
+        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
+            <h1 className="text-xl font-semibold mb-20">{data ? data[0]?.name ?? "No data" : "Loading…"}</h1>
+            <p>lat: {data ? data[0]?.lat ?? "No data" : "Loading…"}</p>
+            <p>lng: {data ? data[0]?.lng ?? "No data" : "Loading…"}</p>
+        </div>
+        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
+          <h1 className="text-xl font-semibold mb-20">{data ? data[1]?.name ?? "No data" : "Loading…"}</h1>
+          <p>lat: {data ? data[1]?.lat ?? "No data" : "Loading…"}</p>
+          <p>lng: {data ? data[1]?.lng ?? "No data" : "Loading…"}</p>
+        </div>
+      </div>
+      <p>Data from API-route</p>
+    </main>
   );
 }
