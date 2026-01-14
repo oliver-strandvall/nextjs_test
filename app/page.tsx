3849cb07d1e8fbf9f4aca12b8f55af5d189ccf65
oliver-strandvall <oliver.strandvall@optistud.fi>
2026-01-14 10:31:06 +0200

    Level 2 Final

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ export default function Home() {
 
   const [data, setData] = useState<{ id: number; name: string; lat: number; lng: number }[] | null>(null);
   const [error, setError] = useState<string | null>(null);
-
+  const [name, setName] = useState("");
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [update, setUpdate] = useState(false);
   async function getData() {
     const res =  await fetch("/api/test/");
-    console.log(res.status)
     if(res.status === 200) {
       setData(await res.json());
     }
@@ -21,8 +23,23 @@ export default function Home() {
   console.log(data);
 
   useEffect(() => {
-    getData();
-  }, []);
+      getData();
+  }, [update]);
+
+  async function handleSubmit(e:React.FormEvent) {
+    e.preventDefault();
+    if(name && lat && lng && lat == Number(lat) && lng == Number(lng)) {
+      const res =  await fetch("/api/test/", {
+        method: "POST",
+        body: JSON.stringify({ name, lat, lng }),
+      })
+      const data = await res.json();
+      setUpdate(!update);
+      console.log("Successfully added location");
+    } else {
+      console.log("Location incorrectly formatted");
+    }
+  };
 
   if(error === "ERROR!") {
   return (
@@ -31,38 +48,29 @@ export default function Home() {
       <div className="bg-red-900 rounded-lg w-110 h-15 flex items-center justify-center">
         <h1 className="text-xl font-semibold">Error loading data</h1>
       </div>
-      <div className="flex items-center justify-center">
-        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
-            <h1 className="text-xl font-semibold mb-20"></h1>
-            <p></p>
-            <p></p>
-        </div>
-        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
-          <h1 className="text-xl font-semibold mb-20"></h1>
-          <p></p>
-          <p></p>
-        </div>
-      </div>
-      <p>Data from API-route</p>
+      <p>Failed to fetch data from API-route</p>
     </main>
   );
   }
 
   return (
-    <main className="bg-blue-950 font-san min-h-screen flex justify-center flex-col items-center gap-5">
+    <main className="bg-blue-950 font-san flex justify-center flex-col items-center gap-5 min-h-screen p-25">
       <h1 className="text-xl font-semibold">Saved Locations:</h1>
-      <div className="flex items-center justify-center">
-        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
-            <h1 className="text-xl font-semibold mb-20">{data ? data[0]?.name ?? "No data" : "Loading…"}</h1>
-            <p>lat: {data ? data[0]?.lat ?? "No data" : "Loading…"}</p>
-            <p>lng: {data ? data[0]?.lng ?? "No data" : "Loading…"}</p>
-        </div>
-        <div className="bg-blue-900 p-4 rounded-lg m-5 w-50 h-50">
-          <h1 className="text-xl font-semibold mb-20">{data ? data[1]?.name ?? "No data" : "Loading…"}</h1>
-          <p>lat: {data ? data[1]?.lat ?? "No data" : "Loading…"}</p>
-          <p>lng: {data ? data[1]?.lng ?? "No data" : "Loading…"}</p>
-        </div>
+      <div className="flex items-center justify-center flex-auto flex-wrap">
+      {data?.map((location) => (
+      <div key ={location.id} className="bg-blue-900 p-5 rounded-lg m-2 w-50 h-50">
+        <h1 className="text-xl font-semibold mb-20">{location.name}</h1>
+        <p>Latitude: {location.lat}</p>
+        <p>Longitude: {location.lng}</p>
       </div>
+      ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Name" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
+        <input onChange={(e)=>setLat(Number(e.target.value))} type="text" placeholder="Latitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
+        <input onChange={(e)=>setLng(Number(e.target.value))} type="text" placeholder="Longitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
+        <button className="bg-blue-600 p-5 rounded-lg hover:bg-blue-700 active:bg-blue-800 block m-auto">Add Loacation</button>
+      </form>
       <p>Data from API-route</p>
     </main>
   );
