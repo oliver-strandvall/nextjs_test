b71e42250e93c4b76c8faa29648810f493eaa7a6
oliver-strandvall <oliver.strandvall@optistud.fi>
2026-02-19 10:22:55 +0200

    Level 4 Init

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,15 @@ export default function Home() {
   const [lat, setLat] = useState("0");
   const [lng, setLng] = useState("0");
 
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
   const [update, setUpdate] = useState(false);
 
   const [addInfo, setAddInfo] = useState("");
+
+  const [loggedIn, setLoggedIn] = useState(false);
 
   async function getData() {
     const res =  await fetch("/api/test/");
@@ -48,6 +54,14 @@ export default function Home() {
     }
   };
 
+  async function handleLogin(e:React.FormEvent) {
+    e.preventDefault();
+    if(userName && password && email) {
+      setLoggedIn(true);
+    }
+  }
+
+  if(loggedIn) {
   if(error) {
   return (
     <main className="bg-blue-950 font-san min-h-screen flex justify-center flex-col items-center gap-5">
@@ -62,32 +76,50 @@ export default function Home() {
 
   return (
     <main className="bg-blue-950 font-san flex justify-center flex-col items-center gap-5 min-h-screen p-25">
-      <h1 className="text-xl font-semibold">Saved Locations:</h1>
+      <h1  className="text-xl font-semibold">Saved Locations:</h1>
       <div className="flex items-center justify-center flex-auto flex-wrap">
       {data?.map((location) => (
-        <Card key={location.id} location={location} onChange={() => setUpdate(!update)} />
+        <Card key={location.id} location={location} onChange={() => setUpdate(!update)} setAddInfo={setAddInfo} />
       ))}
       </div>
-      <div className = {`${addInfo ? "bg-blue-600" : "bg-transparent"} rounded-lg w-110 h-15 flex items-center justify-center`}>
+      <div className = {`${addInfo ? "bg-blue-600" : "bg-transparent"} rounded-lg w-80 h-15 flex items-center justify-center`}>
         <p>{addInfo}</p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Name" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
-        <input onChange={(e)=>setLat(e.target.value)} type="text" placeholder="Latitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
-        <input onChange={(e)=>setLng(e.target.value)} type="text" placeholder="Longitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900"/>
-        <button className="bg-blue-600 p-5 rounded-lg hover:bg-blue-700 active:bg-blue-800 block m-auto">Add Location</button>
+      <form onSubmit={handleSubmit} id="add-form" name="add-form">
+        <div className="flex flex-col items-center gap-2 lg:flex-row lg:gap-4">
+        <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Name" className="m-2 p-2 mb-10 rounded-lg bg-blue-900 w-80 lg:w-60"/>
+        <input onChange={(e)=>setLat(e.target.value)} type="text" placeholder="Latitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900 w-80 lg:w-60"/>
+        <input onChange={(e)=>setLng(e.target.value)} type="text" placeholder="Longitude" className="m-2 p-2 mb-10 rounded-lg bg-blue-900 w-80 lg:w-60"/>
+        </div>
+        <button className="bg-blue-600 p-5 rounded-lg hover:bg-blue-700 active:bg-blue-800 block m-auto w-80 lg:w-60">Add Location</button>
       </form>
       <p>Data from API-route</p>
     </main>
+  );
+} else {
+  return (
+      <main className="bg-blue-950 font-san min-h-screen flex justify-center flex-col items-center">
+        <div className="bg-blue-900 p-5 rounded-lg m-2 w-80 h-80 flex flex-col items-center justify-self-center">
+          <div className="flex flex-wrap items-center h-40 gap-2">
+            <input onChange={(e)=>setUserName(e.target.value)} value={name} type="text" placeholder="Username" className="p-2 rounded-lg bg-blue-950 w-70 h-8"/>
+            <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder="Password" className="p-2 rounded-lg bg-blue-950 w-70 h-8"/>
+          </div>
+        <div className="flex flex-wrap items-center h-40 gap-2">
+          <button type="submit" className="bg-blue-600 p-5 rounded-lg hover:bg-blue-700 active:bg-blue-800 block m-auto w-70">Login</button>
+        </div>
+        </div>
+      </main>
   );
 }
 
 function Card({
   location,
-  onChange
+  onChange,
+  setAddInfo,
 }: {
   location: { id: number; name: string; lat: number; lng: number }, 
-  onChange: () => void
+  onChange: () => void,
+  setAddInfo: (info: string) => void
 }) { 
   const [editing, setEditing] = useState<boolean>(false);
   const [name, setName] = useState(location.name);
@@ -101,6 +133,8 @@ function Card({
       method: "DELETE",
     });
     const data = await res.json();
+    setAddInfo("Successfully deleted location");
+    setTimeout(() => setAddInfo(""), 2500);
     onChange()  
   }
 
@@ -172,4 +206,5 @@ function Card({
       </div>
     </div>
   )
+}
 }
